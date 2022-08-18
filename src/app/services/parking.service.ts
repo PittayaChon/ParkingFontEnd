c5483ef59391859c7bf1deeedb63a91cdeb4bbd3
arigato <teeruch.k@gmail.com>
2022-08-18 23:10:01 +0700

    added url production

--- src/app/services/parking.service.ts
+++ src/app/services/parking.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { StatusParking } from '../variable';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ParkingService {
-  baseUrl = 'http://localhost:1234';
+  baseUrl = environment.production
+    ? 'http://prod.sandbox-me.com:1234/api'
+    : 'http://localhost:1234/api';
 
   constructor(private httpClient: HttpClient) {}
 
