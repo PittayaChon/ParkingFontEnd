17c9b7f206c11c526be98ea6e994f62eb3215265
arigato <teeruch.k@gmail.com>
2022-08-18 22:19:39 +0700

    change property reversable typing

--- src/app/variable.ts
+++ src/app/variable.ts
@@ -1,8 +1,8 @@
 export interface StatusParking {
-  id:number,
-  lot_id: string,
-  licenseplate: string,
-  status: number,
-  reserverble: boolean,
-  floor: string
+  id: number;
+  lot_id: string;
+  licenseplate: string;
+  status: number;
+  reservable: boolean;
+  floor: string;
 }
