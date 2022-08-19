bd962ee8017a7367a622144afc8b2221790adebf
arigato <teeruch.k@gmail.com>
2022-08-19 11:15:37 +0700

    fix added mock data when db disconnect

--- src/app/parking/parking.component.ts
+++ src/app/parking/parking.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { CommandService } from '../command.service';
 import { ParkingService } from '../services/parking.service';
 import { StatusParking } from '../variable';
+import { mockParks } from '../mock';
 
 enum ParkingLotStatus {
   Available = 1,
@@ -18,21 +19,38 @@ enum ParkingLotStatus {
 export class ParkingComponent implements OnInit {
   parks: StatusParking[] = [];
 
-  constructor(private route: Router, private parkingService: ParkingService, private commandService: CommandService) {}
+  constructor(
+    private route: Router,
+    private parkingService: ParkingService,
+    private commandService: CommandService
+  ) {}
 
   ngOnInit(): void {
-    this.parkingService.getParks().subscribe((parks) => {
-      this.parks = parks;
+    this.parkingService.getParks().subscribe({
+      next: (parks) => {
+        if (parks.length) {
+          this.parks = parks;
+        } else {
+          this.parks = mockParks;
+        }
+      },
+      error: (err) => (this.parks = mockParks),
     });
 
     this.commandService.commandMessage.subscribe((socketParkingObj) => {
-      console.log("response from websocket: ", socketParkingObj, typeof(socketParkingObj));
+      console.log(
+        'response from websocket: ',
+        socketParkingObj,
+        typeof socketParkingObj
+      );
 
-      const findParkingIndex = this.parks.findIndex(park => park.id === socketParkingObj.id);
+      const findParkingIndex = this.parks.findIndex(
+        (park) => park.id === socketParkingObj.id
+      );
 
-      this.parks.splice(findParkingIndex, 1, socketParkingObj)
+      this.parks.splice(findParkingIndex, 1, socketParkingObj);
 
-      console.log("updated parks",this.parks);
+      console.log('updated parks', this.parks);
     });
   }
 
@@ -49,35 +67,26 @@ export class ParkingComponent implements OnInit {
   }
 
   updateDisplay(id: number) {
-
-    // send data to websocket
-    // const mockUpdatedPark: StatusParking = {
-    //   id: 1,
-    //   floor: "A",
-    //   lot_id: "A001",
-    //   reservable: false,
-    //   licenseplate: "",
-    //   status: 2
-    // }
-
     const park = this.parks.find((park) => park.id === id);
-    // const park = this.parks.find((park) => park.id === id)|| mockUpdatedPark;
 
     if (park?.reservable) {
       return alert('ช่องสำหรับผู้ที่เป็นสมาชิกเท่านั้น');
     }
-    
+
     const updatedPark = {
       ...park!,
-      status: park!.status === 1 ? 2 : 1,
+      status:
+        park!.status === ParkingLotStatus.Available
+          ? ParkingLotStatus.Occupied
+          : ParkingLotStatus.Available,
     };
-    console.log("updated park", updatedPark);
+    console.log('updated park', updatedPark);
 
-    this.parkingService.updatePark(updatedPark).subscribe((res) => {
-      const index = this.parks.findIndex((park) => park.id === res.id);
+    const index = this.parks.findIndex((park) => park.id === updatedPark.id);
 
-      this.parks[index].status = res.status;
-    });
+    this.parks[index].status = updatedPark.status;
+
+    this.parkingService.updatePark(updatedPark).subscribe(() => {});
 
     this.commandService.commandMessage.next(updatedPark);
   }
